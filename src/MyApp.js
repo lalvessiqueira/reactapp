d0c53c010a52b3f1692cda0419720f523188436f
Leticia Siqueira <lalvessi@calpoly.edu>
2021-01-16 03:40:37 -0300

    item6

--- src/MyApp.js
+++ src/MyApp.js
@@ -11,6 +11,7 @@ function MyApp() {
       const updated = characters.filter((character, i) => {
          return i !== index
       });
+      //const updated = await axios.delete('http://localhost:5000/users', person.data);
       setCharacters(updated);
    }
 
